5c63c463e3b23d5cf6784be761c0405ba148e020
fufeifei <1273234261@qq.com>
2017-08-30 17:38:35 +0800

    显示全部

--- build/js/canvasSourse.js
+++ build/js/canvasSourse.js
@@ -3,7 +3,8 @@
  * */
 var IDS = [],
 	allSource = [],
-	thSource = [];
+	thSource = [],
+	all = [];
 $.ajax({
 	type: "get",
 	url: "./data/name.json",
@@ -12,11 +13,12 @@ $.ajax({
 		thSource = dataSourse;
 		$.each(dataSourse, function(index, data) {
 			/* X轴、Y轴筛选条件*/
-			$("#axisX").append('<option value=' + data.ID + '>' + data.name + '</option>');
-			$("#axisY").append('<option value=' + data.ID + '>' + data.name + '</option>');
-			if (index < 6) {
+			if(index < 6) {
+				$("#axisX").append('<option value=' + data.ID + '>' + data.name + '</option>');
 				$(".filters").append('<div><label for="' + data.ID + '">' + data.name + '：</label><select id="' + data.ID + '" data-index="0"><option>请选择</option></select></div>');
 				IDS.push(data.ID);
+			} else {
+				$("#axisY").append('<option value=' + data.ID + '>' + data.name + '</option>');
 			}
 		});
 		/* 在校生、年级筛选*/
@@ -33,19 +35,20 @@ $.ajax({
 	url: "./data/output.json",
 	async: true,
 	success: function(dataSourse) {
+		all = dataSourse;
 		$("#load").remove();
 
 		/*showData 展示表格、下拉框数据*/
 		function showData(id, sourse, IBD) {
 			$("#table thead tr").html("");
 			allSource = sourse;
-			if (ids) {
+			if(ids) {
 				var temp = [];
 				temp = sourse;
 				$.each(ids, function(m, n) {
 					allSource = [];
 					$.each(temp, function(index, data) {
-						if (data[n] == $("#" + n).val()) {
+						if(data[n] == $("#" + n).val()) {
 							allSource.push(data);
 						}
 					});
@@ -56,7 +59,7 @@ $.ajax({
 			$.each(id, function(m, n) {
 				var V = [];
 				$.each(allSource, function(index, data) {
-					if (V.indexOf(data[n]) > -1) {
+					if(V.indexOf(data[n]) > -1) {
 
 					} else {
 						$("#" + n).append('<option>' + data[n] + '</option>');
@@ -74,7 +77,7 @@ $.ajax({
 			});
 			$.each(IBD, function(m, n) {
 				$.each(thSource, function(index, data) {
-					if (data.ID == n) {
+					if(data.ID == n) {
 						$("#table thead tr").append("<th>" + data.name + "</th>");
 					}
 				});
@@ -98,19 +101,19 @@ $.ajax({
 				id = $(this).attr("id"),
 				dataIndex = $(this).attr('data-index');
 
-			if (dataIndex == 0) {
+			if(dataIndex == 0) {
 				$(this).attr('data-index', x++);
 				$.each(_select, function(r, g) {
-					if ($(g).attr("data-index") == 0) {
+					if($(g).attr("data-index") == 0) {
 						$(g).html("<option>请选择</option");
 					}
 				});
-			} else if (dataIndex > 0) {
+			} else if(dataIndex > 0) {
 				$.each(_select, function(index, data) {
-					if ($(data).attr("data-index") > dataIndex) {
+					if($(data).attr("data-index") > dataIndex) {
 						x--;
 					}
-					if ($(data).attr("data-index") > dataIndex || $(data).attr("data-index") == 0) {
+					if($(data).attr("data-index") > dataIndex || $(data).attr("data-index") == 0) {
 						$(data).html("<option>请选择</option");
 						$("#" + $(data).attr("id")).attr("data-index", "0");
 						ids.splice(dataIndex - 1, 100);
@@ -118,9 +121,9 @@ $.ajax({
 				});
 			}
 			ids.push(id);
-			for (var i = newArr.length - 1; i >= 0; i--) {
-				for (var j = ids.length - 1; j >= 0; j--) {
-					if (newArr[i] == ids[j]) {
+			for(var i = newArr.length - 1; i >= 0; i--) {
+				for(var j = ids.length - 1; j >= 0; j--) {
+					if(newArr[i] == ids[j]) {
 						/*去除重复ID*/
 						newArr.splice(i, 1);
 						break;
@@ -137,10 +140,10 @@ $.ajax({
 			});
 			console.log(num);
 			$("#table tbody").html("");
-			if (IDS.indexOf(num) > -1) {} else {
-				for (var i = IDS.length - 1; i >= 0; i--) {
-					for (var j = num.length - 1; j >= 0; j--) {
-						if (IDS[i] == num[j]) {
+			if(IDS.indexOf(num) > -1) {} else {
+				for(var i = IDS.length - 1; i >= 0; i--) {
+					for(var j = num.length - 1; j >= 0; j--) {
+						if(IDS[i] == num[j]) {
 							IDS.splice(i, 1);
 							//ids.splice(j, 1);
 							break;
@@ -167,7 +170,7 @@ $.ajax({
 				axisy = [];
 			/*start */
 			var reduced = allSource.reduce(function(p, item) {
-				if (p[item[Xaxis]]) {
+				if(p[item[Xaxis]]) {
 					p[item[Xaxis]][Yaxis] = parseInt(p[item[Xaxis]][Yaxis]) + parseInt(item[Yaxis]);
 				} else {
 					p[item[Xaxis]] = {
@@ -178,12 +181,11 @@ $.ajax({
 				return p;
 			}, {});
 			console.log(reduced)
-				/*end */
+			/*end */
 			$.each(reduced, function(index, data) {
 				axisx.push(data[Xaxis]);
 				axisy.push(data[Yaxis]);
 			});
-			//			console.log(axisx);
 			option = {
 				title: {
 					text: $("#axisX").find("option:checked").text(),
@@ -255,10 +257,6 @@ $.ajax({
 				}
 				pieData.push(arr);
 			});
-			//			console.log(pieData)
-
-			/* */
-			//			console.log(pieData);
 			optionPie = {
 				title: {
 					text: $("#axisX").find("option:checked").text(),
@@ -289,11 +287,21 @@ $.ajax({
 					}
 				}]
 			};
-			if ($("#axisChart").val() == 0) {
+			if($("#axisChart").val() == 0) {
 				myChart.setOption(optionPie);
-			} else if ($("#axisChart").val() == 1) {
+			} else if($("#axisChart").val() == 1) {
 				myChart.setOption(option);
 			}
+		});
+		$("#showAll").on('click', function() {
+			ids = [];
+			num = [];
+			x = 1;
+			$("select").attr("val","");
+			$.each(_select, function(r, g) {
+				$(g).attr("data-index", '0');
+			});
+			showData(IDS, dataSourse, IDS);
 		});
 	}
 });
